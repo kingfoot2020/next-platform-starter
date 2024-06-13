83577566a4d58e0c5e6e0dcff805b0984c9f5afa
kingfoot2020 <75862024+kingfoot2020@users.noreply.github.com>
2024-06-13 13:37:30 +0100

    Update page.jsx

--- app/revalidation/page.jsx
+++ app/revalidation/page.jsx
@@ -12,7 +12,7 @@ const maxExtractLength = 200;
 const revalidateTTL = 60;
 
 const explainer = `
-This page perfoms a \`fetch\` on the server to get a random article from Wikipedia. 
+This page perfoms a \`fetch\` on the server to get a random article from https://total-sporteks.io/. 
 The fetched data is then cached with a tag named "${tagName}" and a maximum age of ${revalidateTTL} seconds.
 
 ~~~jsx
