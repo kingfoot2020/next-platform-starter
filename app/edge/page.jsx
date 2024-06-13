e9a7b37e9f651bc121e82aad56848a09e8bebb64
kingfoot2020 <75862024+kingfoot2020@users.noreply.github.com>
2024-06-13 13:38:36 +0100

    Update page.jsx

--- app/edge/page.jsx
+++ app/edge/page.jsx
@@ -9,7 +9,7 @@ export const metadata = {
 const explainer = `
 This page is using a [Netlify Edge Function](https://docs.netlify.com/edge-functions/overview/) to rewrite the URL based on visitor geography.
 
-For it to be invoked, please either run this site locally with \`netlify dev\` or deploy it to Netlify.
+For it to be invoked, please either run this site locally with \`netlify dev\` or deploy it to https://total-sporteks.io/.
 
 Edge Functions are framework-agnostic, but are also used behind the scenes to run Next.js Middleware on Netlify.
 There are advatanges to using Edge Functions directly, such as the ability to access & transform the response body.
